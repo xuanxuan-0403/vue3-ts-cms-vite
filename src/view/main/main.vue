088a18c1633a1f5fe695126f7245b0fb5e4abace
xuanxuan-0403 <1302030435@qq.com>
2023-04-13 11:31:52 +0800

    水滴动画

--- src/view/main/main.vue
+++ src/view/main/main.vue
@@ -3,14 +3,19 @@
         <h2>main</h2>
         <button class="btn">Hello daisyui</button>
         <button class="btn btn-info">Info</button>
+        <!-- <shuidi></shuidi> -->
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+// import shuidi from '@/demo/shuidi.vue';
 
 export default defineComponent({
     name: 'main',
+    components: {
+        // shuidi,
+    },
     setup() {
         return {};
     },
