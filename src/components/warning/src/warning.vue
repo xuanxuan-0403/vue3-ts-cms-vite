e91ac0c6444cc721c1f33d86309e44040cddc5e6
xuanxuan-0403 <1302030435@qq.com>
2023-07-04 17:25:05 +0800

    feat(版权声明): 版权声明

--- src/components/warning/src/warning.vue
+++ src/components/warning/src/warning.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="alert alert-warning shadow-lg h-14">
+    <div class="alert alert-warning shadow-lg h-14" v-if="isShow">
         <div>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -15,18 +15,46 @@
                 />
             </svg>
             <span><slot>请填写内容! </slot></span>
+            <button class="btn btn-square btn-outline" @click="handleShowBtn">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
+                    />
+                </svg>
+            </button>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
     setup() {
-        return {};
+        const isShow = ref(true);
+        const handleShowBtn = () => {
+            isShow.value = false;
+        };
+        return {
+            isShow,
+            handleShowBtn,
+        };
     },
 });
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.btn {
+    position: absolute;
+    right: 2%;
+}
+</style>
--- src/view/display/display.vue
+++ src/view/display/display.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="display">
         <CardContainer :is-show="true" :table-data="tableData" />
+
+        <transition name="copyrightTran" mode="out-in" appear>
+            <Warning class="copyright">
+                开发者: 谢亚轩 | 开发时间: 2023.04-至今 | github:
+                https://github.com/xuanxuan-0403</Warning
+            >
+        </transition>
     </div>
 </template>
 
@@ -8,6 +15,7 @@
 import { defineComponent, computed } from 'vue';
 import CardContainer from '../main/components/card-container/card-container.vue';
 import type { ITable } from '@/service/main/system/types';
+import Warning from '@/components/warning';
 
 import { useStore } from '@/store';
 const store = useStore();
@@ -15,6 +23,7 @@ const store = useStore();
 export default defineComponent({
     components: {
         CardContainer,
+        Warning,
     },
     setup() {
         const { display } = store;
@@ -30,11 +39,34 @@ export default defineComponent({
 
 <style lang="less">
 .display {
+    position: relative;
     transition: all 0.5s;
     width: 100vw;
     height: 100vh;
     padding: 5vh 8vw;
     overflow: hidden;
     background-color: #eff0f5;
+
+    .copyright {
+        width: 80vw;
+        position: absolute;
+        top: 2%;
+        z-index: 99999999;
+    }
+}
+
+.copyrightTran-enter-from,
+.copyrightTran-leave-to {
+    transform: translateY(-200%);
+}
+
+.copyrightTran-enter-to,
+.copyrightTran-leave-from {
+    transform: translateY(0);
+}
+
+.copyrightTran-enter-active,
+.copyrightTran-leave-active {
+    transition: all 1s ease;
 }
 </style>
