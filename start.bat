11db52101c2278cb16acf98dd3678474f906e46f
xuanxuan-0403 <1302030435@qq.com>
2023-10-07 10:45:24 +0800

    feat(public): 增加一键运行项目的start.bat

---
+++ start.bat
@@ -0,0 +1,1 @@
+start cmd /k "cd E:\I_Project\Vue-project\vue3-ts-cms-vite && E: && npm run dev"
--- vite.config.ts
+++ vite.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
     },
     server: {
         https: false, // 是否开启 https
-        open: false, // 是否自动在浏览器打开
+        open: true, // 是否自动在浏览器打开
         cors: true, // 允许跨域
         port: 3000, // 端口号
         host: '0.0.0.0',
